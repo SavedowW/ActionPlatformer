f61f1406a56cdd4a03991a631e7522df0a096e0a
DeadM00n <velikanov-em@yandex.ru>
2026-05-25 01:00:40 +0300

    Timeline property changed after testing

--- src/Core/TimelineProperty.hpp
+++ src/Core/TimelineProperty.hpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <map>
 #include <vector>
+#include <format>
 
 /*
     For each key K find values V1 where K1 is the greatest stored key with K1 < K
@@ -27,250 +28,223 @@ template<typename T>
 class TimelineProperty {
 public:
     // Requires copying due to std::map - inevitable because for nice syntax you need to build std::initializer_list
-    TimelineProperty(const std::map<uint32_t, T> &values_) :
-        m_isEmpty(false)
+    TimelineProperty(const std::map<uint32_t, T> &values_)
     {
         if (values_.empty())
             throw std::runtime_error("Trying to create a timeline property from an empty map");
 
-        m_keys.reserve(values_.size());
-        m_values.reserve(values_.size());
+        m_data.reserve(values_.size());
 
         for (const auto &el : values_)
-        {
-            m_keys.emplace_back(el.first);
-            m_values.emplace_back(el.second);
-        }
+            m_data.emplace_back(el);
 
-        m_last = 0;
-        m_lastRequest = m_keys[m_last];
-    }
-
-    // Considered empty, but actually creates a default constructed element at 0
-    TimelineProperty()
-    {
-        m_keys.emplace_back(0);
-        m_values.emplace_back();
+        m_lastRequest = m_data.end();
     }
 
     // Creates at 0, requires only move-constructor
     TimelineProperty(T &&rhs_) :
-        m_isEmpty(false)
-    {
-        m_keys.emplace_back(0);
-        m_values.emplace_back(std::move(rhs_));
-    }
+        m_data{{0, std::move(rhs_)}},
+        m_lastRequest{m_data.begin()}
+    {}
 
-    // Requires copy-constructor
-    TimelineProperty(const TimelineProperty<T> &rhs_)
-    {
-        m_keys = rhs_.m_keys;
-        m_values = rhs_.m_values;
-        m_last = 0;
-        m_lastRequest = m_keys[m_last];
-        m_isEmpty = rhs_.m_isEmpty;
-    }
+    TimelineProperty(const T &rhs_) :
+        m_data{{0, rhs_}},
+        m_lastRequest{m_data.begin()}
+    {}
 
-    // Requires copy-constructor
-    TimelineProperty& operator=(const TimelineProperty<T> &rhs_)
-    {
-        m_keys = rhs_.m_keys;
-        m_values = rhs_.m_values;
-        m_last = 0;
-        m_lastRequest = m_keys[m_last];
-        m_isEmpty = rhs_.m_isEmpty;
+    TimelineProperty() = default;
 
-        return *this;
-    }
+    TimelineProperty(const TimelineProperty<T> &rhs_) = delete;
+    TimelineProperty& operator=(const TimelineProperty<T> &rhs_) = delete;
 
     // Requires only move constructor
-    TimelineProperty(TimelineProperty<T> &&rhs_) noexcept
-    {
-        m_keys = std::move(rhs_.m_keys);
-        m_values = std::move(rhs_.m_values);
-        m_last = 0;
-        m_lastRequest = m_keys[m_last];
-        m_isEmpty = rhs_.m_isEmpty;
-
-        rhs_.m_last = 0;
-        rhs_.m_lastRequest = 0;
-        rhs_.m_isEmpty = true;
-    }
+    TimelineProperty(TimelineProperty<T> &&rhs_) noexcept :
+        m_data{std::move(rhs_.m_data)},
+        m_lastRequest{m_data.begin()}
+    {}
 
     // Requires only move constructor
     TimelineProperty& operator=(TimelineProperty<T> &&rhs_) noexcept
     {
-        m_keys = std::move(rhs_.m_keys);
-        m_values = std::move(rhs_.m_values);
-        m_last = 0;
-        m_lastRequest = m_keys[m_last];
-        m_isEmpty = rhs_.m_isEmpty;
-
-        rhs_.m_last = 0;
-        rhs_.m_lastRequest = 0;
-        rhs_.m_isEmpty = true;
-
+        m_data = std::move(rhs_.m_data);
+        m_lastRequest = m_data.begin();
         return *this;
     }
 
     bool isEmpty() const noexcept
     {
-        return m_isEmpty;
+        return m_data.empty();
     }
 
     // Requires move constructor and assignment
     TimelineProperty &addPair(uint32_t timeMark_, T &&rhs_)
     {
-        // TODO: what if timeMark_ < min?
-        if (m_isEmpty)
-        {
-            m_keys = {timeMark_};
-            m_values.clear();
-            m_values.emplace_back(std::move(rhs_));
-
-            m_last = 0;
-            m_lastRequest = m_keys[m_last];
-            m_isEmpty = false;
-        }
+        if (m_data.empty())
+            m_data.emplace_back(timeMark_, std::move(rhs_));
         else
         {
-            size_t id = binarySearch(timeMark_);
-            if (m_keys[id] == timeMark_)
-            {
-                m_values[id] = std::move(rhs_);
-
-                m_lastRequest = m_keys[m_last];
-            }
+            if (timeMark_ < m_data.front().first)
+                m_data.emplace(m_data.begin(), timeMark_, std::move(rhs_));
             else
             {
-                id++;
-
-                m_keys.emplace(m_keys.begin() + id, timeMark_);
-                m_values.emplace(m_values.begin() + id, std::move(rhs_));
-
-                m_lastRequest = m_keys[m_last];
+                const auto it = binarySearch(timeMark_);
+                if (it->first == timeMark_)
+                    it->second = std::move(rhs_);
+                else
+                    m_data.emplace(it + 1, timeMark_, std::move(rhs_));
             }
         }
 
+        m_lastRequest = m_data.begin();
         return *this;
     }
 
     // Requires copy constructor and assignment
     TimelineProperty &addPair(uint32_t timeMark_, const T &rhs_)
     {
-        // TODO: what if timeMark_ < min?
-        if (m_isEmpty)
-        {
-            m_keys = {timeMark_};
-            m_values.clear();
-            m_values.emplace_back(rhs_);
-
-            m_last = 0;
-            m_lastRequest = m_keys[m_last];
-            m_isEmpty = false;
-        }
+        if (m_data.empty())
+            m_data.emplace_back(timeMark_, rhs_);
         else
         {
-            size_t id = binarySearch(timeMark_);
-            if (m_keys[id] == timeMark_)
-            {
-                m_values[id] = rhs_;
-
-                m_lastRequest = m_keys[m_last];
-            }
+            if (timeMark_ < m_data.front().first)
+                m_data.emplace(m_data.begin(), timeMark_, rhs_);
             else
             {
-                id++;
-
-                m_keys.emplace(m_keys.begin() + id, timeMark_);
-                m_values.emplace(m_values.begin() + id, rhs_);
-
-                m_lastRequest = m_keys[m_last];
+                const auto it = binarySearch(timeMark_);
+                if (it->first == timeMark_)
+                    it->second = rhs_;
+                else
+                    m_data.emplace(it + 1, timeMark_, rhs_);
             }
         }
 
+        m_lastRequest = m_data.begin();
         return *this;
     }
 
-    const T &operator[](uint32_t timeMark_) const requires (!std::same_as<T, bool>)
+    const T &operator[](uint32_t timeMark_) const
     {
         iterateLastTo(timeMark_);
-        return m_values.at(m_last);
-    }
-
-    T operator[](uint32_t timeMark_) const requires (std::same_as<T, bool>)
-    {
-        iterateLastTo(timeMark_);
-        return m_values.at(m_last);
+        return m_lastRequest->second;
     }
 
 protected:
     void iterateLastTo(uint32_t timeMark_) const
     {
-        if (m_keys.empty() || m_values.empty())
-            throw std::runtime_error("Trying to index a moved-from timeline");
+        if (m_data.empty())
+            throw std::runtime_error("Trying to index an empty timeline");
 
-        int64_t diff = static_cast<int64_t>(timeMark_) - m_lastRequest;
-        m_lastRequest = timeMark_;
+        const int64_t diff = static_cast<int64_t>(timeMark_) - m_lastRequest->first;
 
         // Dirty, but only 2 conditions at most in probably the most important function in the project
         if (diff > 0)
         {
             if (diff <= 5)
             {
-                while (m_last != m_keys.size() - 1 && m_keys[m_last + 1] <= timeMark_)
-                    m_last++;
+                while (m_lastRequest != m_data.end() - 1 && (m_lastRequest + 1)->first <= timeMark_)
+                    ++m_lastRequest;
             }
             else
-                m_last = binarySearch(timeMark_);
+                m_lastRequest = binarySearch(timeMark_);
         }
         else if (diff < 0)
         {
             if (diff >= -5)
             {
-                while (m_last != 0 && m_keys[m_last] > timeMark_)
-                    m_last--;
+                while (m_lastRequest != m_data.begin() && m_lastRequest->first > timeMark_)
+                    m_lastRequest--;
             }
             else
-                m_last = binarySearch(timeMark_);
+                m_lastRequest = binarySearch(timeMark_);
         }
     }
 
-    size_t binarySearch(uint32_t timeMark_) const
+    std::vector<std::pair<uint32_t, T>>::const_iterator binarySearch(const uint32_t timeMark_) const
     {
-        const auto size = m_keys.size();
-        if (timeMark_ >= m_keys[size - 1])
-        {
-            return size - 1;
-        }
+        const auto size = m_data.size();
+        if (timeMark_ >= m_data[size - 1].first)
+            return m_data.cbegin() + (size - 1);
 
-        if (timeMark_ < m_keys[0])
-        {
-            return 0;
-        }
+        if (timeMark_ <= m_data[0].first)
+            return m_data.cbegin();
 
         size_t lBound = 0, rBound = size - 2;
 
         while (rBound - lBound > 1)
         {
             size_t idxBetween = (rBound + lBound) / 2;
-            if (timeMark_ >= m_keys[idxBetween])
+            if (timeMark_ >= m_data[idxBetween].first)
                 lBound = idxBetween;
             else
                 rBound = idxBetween - 1;
         }
 
-        if (m_keys[rBound] <= timeMark_)
-            return rBound;
-        else
-            return lBound;
+        if (m_data[rBound].first <= timeMark_)
+            return m_data.cbegin() + rBound;
+
+        return m_data.cbegin() + lBound;
     }
 
-    std::vector<uint32_t> m_keys;
-    std::vector<T> m_values;
-    mutable uint32_t m_lastRequest = 0;
-    mutable size_t m_last = 0;
-    bool m_isEmpty = true;
+    std::vector<std::pair<uint32_t, T>>::iterator binarySearch(const uint32_t timeMark_)
+    {
+        const auto size = m_data.size();
+        if (timeMark_ >= m_data[size - 1].first)
+            return m_data.begin() + (size - 1);
+
+        if (timeMark_ <= m_data[0].first)
+            return m_data.begin();
+
+        size_t lBound = 0, rBound = size - 2;
+
+        while (rBound - lBound > 1)
+        {
+            size_t idxBetween = (rBound + lBound) / 2;
+            if (timeMark_ >= m_data[idxBetween].first)
+                lBound = idxBetween;
+            else
+                rBound = idxBetween - 1;
+        }
+
+        if (m_data[rBound].first <= timeMark_)
+            return m_data.begin() + rBound;
+
+        return m_data.begin() + lBound;
+    }
+
+    std::vector<std::pair<uint32_t, T>> m_data;
+    mutable std::vector<std::pair<uint32_t, T>>::const_iterator m_lastRequest = m_data.end();
 
     static constexpr int m_stepThreshold = 5;
 };
+
+/**
+ *  NOTE: after several tests, it seems to be the fastest implementation for sequential access back-and-forth. Test results:
+ * 
+ *                                              filling    readRandom      readSequential
+ *                                              
+ * Int:
+ * TimelinePropertyIndividualId:                24'000     43'217'700      22'889'100
+ * TimelinePropertyIndividualIterator:          51'600     47'618'000      23'461'700
+ * TimelinePropertyIndividualIteratorAlgorithm: 62'500     26'861'800      23'768'100
+ * TimelinePropertySingleId:                    58'000     44'106'500      23'934'500
+ * TimelinePropertySingleIterator:              45'300     45'921'700      22'647'900
+ * TimelinePropertySingleIteratorAlgorithm:     55'800     23'253'100      22'194'300
+ * TimelinePropertyMap:                         109'700    54'219'800      64'079'600
+ * 
+ * String:
+ * TimelinePropertyIndividualId:                89'000     47'445'200      26'232'500
+ * TimelinePropertyIndividualIterator:          99'000     44'769'700      27'240'900
+ * TimelinePropertyIndividualIteratorAlgorithm: 104'900    26'214'200      26'353'500
+ * TimelinePropertySingleId:                    111'700    45'465'600      26'853'500
+ * TimelinePropertySingleIterator:              111'500    47'690'700      21'805'200
+ * TimelinePropertySingleIteratorAlgorithm:     97'900     47'647'900      23'903'000
+ * TimelinePropertyMap:                         159'600    56'887'000      73'036'900
+ *
+ * Here:
+ *  - "Individual" - separate vectors for keys and values
+ *  - "Single" - one vector of pairs for keys and values
+ *  - "ID" - caching vector indices
+ *  - "Iterator" - caching vector iterators (and using them for filling)
+ *  - "Algorithm" - using upper_bound instead of custom implementation
+ *  - TimelinePropertyMap used iterators and algo
+ */
